f34a7667ae015c8f727c61a4bfcfb58408929109
zfachrian <zuhad.shoesmart@gmail.com>
2021-01-08 18:16:59 +0700

    add scan data score for view

--- MyScores.java
+++ MyScores.java
@@ -7,6 +7,23 @@ public class MyScores implements StudentQuizManagement {
 	@Override
 	public void QuizAction() {
 		System.out.println("View My Scores");
+
+		Scanner scan = new Scanner(System.in);
+
+		File file = new File(Login.currentName + ".txt");
+		Scanner input;
+		try {
+			input = new Scanner(file);
+			int i = 0;
+			while (input.hasNext()) {
+				i++;
+				String word = input.nextLine();
+				System.out.println("Test ke-" + i + " : " + word);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
