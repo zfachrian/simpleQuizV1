554c4cd13e4c4b4b1e1df73c1d1fbd55f28f9780
zfachrian <zuhad.shoesmart@gmail.com>
2021-01-09 15:38:17 +0700

    import lib scaner

--- ModifyQuiz.java
+++ ModifyQuiz.java
@@ -1,9 +1,16 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
-public class ModifyQuiz implements ProfessorQuiz{
+public class ModifyQuiz implements ProfessorQuiz {
 
 	@Override
 	public void Quiz() {
 		System.out.println("Inside Professor modify quiz");
+
 	}
 
 }
