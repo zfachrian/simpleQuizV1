78a5fc5a1980f568bdd07094da6df114e2c3c6e2
zfachrian <zuhad.shoesmart@gmail.com>
2021-01-10 17:44:46 +0700

    import lib

--- ModifyAccount.java
+++ ModifyAccount.java
@@ -1,9 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class ModifyAccount implements LecturerAccount {
 
 	@Override
 	public void Account() {
 		System.out.println("Lecturer Modify Account");
+		Main.currentAccountInfo();
 	}
 
 }
